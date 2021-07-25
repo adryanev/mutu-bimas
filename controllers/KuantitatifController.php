0c5134fdf10b7281468d676d707638ec2285f195
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-07-25 11:36:21 +0700

    fallback ketika tidak terhubung dengan API

--- controllers/KuantitatifController.php
+++ controllers/KuantitatifController.php
@@ -3,7 +3,6 @@
 
 namespace app\controllers;
 
-
 use common\models\kriteria9\akreditasi\K9AkreditasiProdi;
 use yii\base\BaseObject;
 use yii\data\ArrayDataProvider;
@@ -12,38 +11,37 @@ use yii\web\NotFoundHttpException;
 class KuantitatifController extends BaseApiController
 {
 
-    public function actionDownloadProdi($id,$institusi)
+    public function actionDownloadProdi($id, $institusi)
     {
         $modelInstitusi = $this->findInstitusi($institusi);
         $aplikasi = $modelInstitusi->aplikasi;
 
-        $response = $this->sendRequest($aplikasi,'kuantitatif','download-dokumen',['dokumen'=>$id]);
-        $download = $this->downloadFiles($response->data->path,$response->data->filename);
+        $response = $this->sendRequest($aplikasi, 'kuantitatif', 'download-dokumen', ['dokumen'=>$id]);
+        $download = $this->downloadFiles($response->data->path, $response->data->filename);
         return \Yii::$app->response->sendFile($download);
     }
 
-    public function actionShow($id,$institusi)
+    public function actionShow($id, $institusi)
     {
 
         $modelInstitusi = $this->findInstitusi($institusi);
         $aplikasi = $modelInstitusi->aplikasi;
 
-        $response = $this->sendRequest($aplikasi,'kuantitatif','show',['id'=>$id]);
+        $response = $this->sendRequest($aplikasi, 'kuantitatif', 'show', ['id'=>$id]);
 
-        return $this->renderAjax('//lk/_modal_content',[
+        return $this->renderAjax('//lk/_modal_content', [
             'response'=>$response->data,
         ]);
     }
 
-    public function actionProdi($id,$institusi)
+    public function actionProdi($id, $institusi)
     {
 
         $modelInstitusi = $this->findInstitusi($institusi);
         $aplikasi = $modelInstitusi->aplikasi;
 
-        $response = $this->sendRequest($aplikasi,'kuantitatif','prodi',['id'=>$id]);
+        $response = $this->sendRequest($aplikasi, 'kuantitatif', 'prodi', ['id'=>$id]);
         $dataProvider = new ArrayDataProvider(['allModels'=>$response->data->items]);
         return $this->renderAjax('_list', ['untuk'=>'prodi','dataProvider'=>$dataProvider,'institusi'=>$modelInstitusi]);
-
     }
 }
